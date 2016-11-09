280a5381340d609ffa9b035cfadc02f9a2e432a6
Andrew Zhao <snareclaw@gmail.com>
2016-11-09 00:10:44 -0800

    Add navbar template and logo assets

--- shoereo-app/main.js
+++ shoereo-app/main.js
@@ -14,5 +14,28 @@
  *     See the License for the specific language governing permissions and
  *     limitations under the License.
  */
-trace("Hello, World!\n");
-debugger;
+
+let navbarSkin = new Skin({ fill: "#4F4F4F" });
+let backgroundSkin = new Skin({ fill: "#333333" });
+let navbarBackStyle = new Style({ font: "bold 30px", color: "#F2F2F2" })
+
+let Navbar = Line.template($ => ({
+   left: 0, right: 0, top: 0,
+   height: 40,
+   skin: navbarSkin,
+   contents: [
+     Label($, {left:5, top: 5, bottom: 5, string: "<", style: navbarBackStyle}),
+     Picture($, {left: 90, right: 90, url: "assets/navbarLogo.png", height: 30}),
+   ],
+}));
+
+let PerformanceContainer = Column.template($ => ({
+    left: 0, right: 0, top: 0, bottom: 0, skin: backgroundSkin,
+    name: 'performanceContainer',
+    contents: [
+        new Navbar()
+    ]
+}));
+
+let mainContainer = new PerformanceContainer();
+application.add(mainContainer);
