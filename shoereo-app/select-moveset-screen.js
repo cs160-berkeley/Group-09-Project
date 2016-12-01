e0e0b7079e756875778dbb69957b11f99597023a
Daniel Kim <dankim913@gmail.com>
2016-12-01 00:32:16 -0800

    modularized navbar

--- shoereo-app/select-moveset-screen.js
+++ shoereo-app/select-moveset-screen.js
@@ -1,36 +1,8 @@
 /* === IMPORT STATEMENTS === */
-import { changeScreen, backScreen, switchTitleScreen } from "main";
+import { changeScreen } from "main";
 import { newMovesetScreen } from "new-moveset-screen";
 import { performanceScreen } from "performance-screen";
-
-/* === NAVBAR === */
-
-let navbarSkin = new Skin({ fill: "#4F4F4F" });
-let navbarBackStyle = new Style({ font: "bold 30px", color: "#F2F2F2" });
-
-var Navbar = Line.template($ => ({
-    left: 0, right: 0, top: 0,
-    height: 40,
-    skin: navbarSkin,
-    contents: [
-      Label($, {
-        left: 5, top: 5, bottom: 5, active: true, string: "<", style: navbarBackStyle,
-        behavior: Behavior({
-          onTouchEnded: function(content) {
-            backScreen();
-          }
-        })
-      }),
-      Picture($, {
-        left: 90, right: 90, url: "assets/navbarLogo.png", height: 30, active: true,
-        behavior: Behavior({
-          onTouchEnded: function(content) {
-            switchTitleScreen();
-          }
-        })
-      }),
-    ],
-}));
+import { Navbar } from "navbar";
 
 /* === SELECT MOVESET SCREEN === */
 
