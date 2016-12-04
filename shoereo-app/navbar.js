0aebcd0f3972b84ebf0985f1139496aabff8e2b8
Daniel Kim <dankim913@gmail.com>
2016-12-04 00:13:31 -0800

    new background, navbar stuff

--- shoereo-app/navbar.js
+++ shoereo-app/navbar.js
@@ -3,8 +3,64 @@ import { backScreen, switchTitleScreen } from "main";
 
 /* === NAVBAR === */
 
-let navbarSkin = new Skin({ fill: "#4F4F4F" });
-let navbarBackStyle = new Style({ font: "bold 30px", color: "#F2F2F2" });
+let navbarSkin = new Skin({ fill: "#333333" });
+let navbarBackStyle = new Style({ font: "30px", color: "#FFFFFF" });
+
+export var Navba1r = Line.template($ => ({
+    left: 0, right: 0, top: 0,
+    height: 40,
+    skin: navbarSkin,
+    contents: [
+      Label($, {
+        left: 5, top: 5, bottom: 5, active: true, string: "<", style: navbarBackStyle,
+        behavior: Behavior({ 
+          onTouchEnded: function(content) {
+            backScreen();
+          }
+        })
+      }),
+      Picture($, {
+        left: 90, right: 90, url: "assets/navbar/navbarLogo.png", height: 20, active: true,
+        behavior: Behavior({
+          onTouchEnded: function(content) {
+            switchTitleScreen();
+          }
+        })
+      }),
+    ],
+}));
+
+export var NavbarDelete = Line.template($ => ({
+    left: 0, right: 0, top: 0,
+    height: 40,
+    skin: navbarSkin,
+    contents: [
+      Label($, {
+        left: 5, top: 5, bottom: 5, active: true, string: "<", style: navbarBackStyle,
+        behavior: Behavior({ 
+          onTouchEnded: function(content) {
+            backScreen();
+          }
+        })
+      }),
+      Picture($, {
+        left: 90, right: 90, url: "assets/navbar/navbarLogo.png", height: 20, active: true,
+        behavior: Behavior({
+          onTouchEnded: function(content) {
+            switchTitleScreen();
+          }
+        })
+      }),
+      Picture($, {
+        right: 10, url: "assets/navbar/delete.png", height: 25, active: true,
+        behavior: Behavior({
+          onTouchEnded: function(content) {
+            switchTitleScreen();
+          }
+        })
+      })
+    ],
+}));
 
 export var Navbar = Line.template($ => ({
     left: 0, right: 0, top: 0,
@@ -20,12 +76,28 @@ export var Navbar = Line.template($ => ({
         })
       }),
       Picture($, {
-        left: 90, right: 90, url: "assets/navbarLogo.png", height: 30, active: true,
+        left: 90, right: 90, url: "assets/navbar/navbarLogo.png", height: 20, active: true,
         behavior: Behavior({
           onTouchEnded: function(content) {
             switchTitleScreen();
           }
         })
       }),
+      Picture($, {
+        right: 30, url: "assets/navbar/share.png", height: 25, active: true,
+        behavior: Behavior({
+          onTouchEnded: function(content) {
+            switchTitleScreen();
+          }
+        })
+      }),
+      /*Picture($, {
+        right: 10, url: "assets/navbar/delete.png", height: 25, active: true,
+        behavior: Behavior({
+          onTouchEnded: function(content) {
+            switchTitleScreen();
+          }
+        })
+      })*/
     ],
 }));
